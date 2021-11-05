9c6445821602175d63f5f7d9e1f04ff52795b8de
michikun06 <michikun06@icloud.com>
2021-11-05 19:37:39 +0900

    デフォルト引数付きメソッド、真偽値を判定するメソッド、破壊的メソッドについてまとめた

--- Chapter2_Basic_of_ruby/method.rb
+++ Chapter2_Basic_of_ruby/method.rb
@@ -18,4 +18,31 @@ p add(2, 6)
 # ()の使い方について
 ## rubyではメソッド名の後の()は省略できる。引数がある場合には()を使用して、ない場合には（）を使用しないのが一般的
 
+# デフォルト付きの引数
+## rubyではメソッドを呼び出す際に引数の過不足があるとエラーになるためデフォルトで引数を与えることができる
+def foo(time = Time.now, message = "bar")
+    puts "time : #{time}, message : #{message}"
+end
+foo
 
+# ?で終わるメソッド
+## Rubyでは?で終了するメソッドでは慣習として真偽値を返すメソッド（trueかfalseを返す）となる。
+## 真偽値を返すメソッドであれば?で終わらせるようにする。
+def multiple_of_three?(n = 3)
+    n % 3 == 0
+end
+puts multiple_of_three?
+puts multiple_of_three?(4)
+
+# !で終わるメソッド
+## Rubyでは!で終了するメソッドでは慣習として「使用する際は注意が必要」という意味を持つ
+## 例えば下記メソッド。Stringオブジェクトには「upcase」と「upcase!」の２種類がある。
+## どちらも大文字にするという意味を持っているが、「upcase!」メソッドは呼び出した文字列自身も変更するという意味を持つ。
+## 呼び出し元の変数も変更させるメソッドを「破壊的メソッド」という。メソッドに「!」がついていた場合はAPIドキュメントを読んで確認する。
+s = "ruby"
+def reverse_upcase!(name)
+    name.reverse!.upcase!
+end
+puts s     ## 変数宣言通りrubyが格納されている
+puts reverse_upcase!(s)     ## メソッドによりreverseしてupcaseしている
+puts s     ## 破壊的メソッドにより、reverseしてupcaseした状態になっている
