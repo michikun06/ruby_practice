b7642bd5ced942331aba66a6c263873d7b358954
michikun06 <michikun06@icloud.com>
2022-01-07 12:10:25 +0900

    ワードシンセサイズプログラムを完成させた

--- Chapter10_yield_and_Proc/lib/word_synth.rb
+++ Chapter10_yield_and_Proc/lib/word_synth.rb
@@ -1,4 +1,16 @@
 class WordSynth
-    def play(oroginal_words)
+    def initialize
+        @effects = []
+    end
+
+    # 引数のエフェクトを@effects配列に追加していくメソッド
+    def add_effect(effect)
+        @effects << effect
+    end
+
+    def play(original_words)
+        @effects.inject(original_words) do |words, effect|
+            effect.call(words)
+        end
     end
 end
